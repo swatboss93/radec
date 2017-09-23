754d8f2fbed0ffcf565cfde1641db31fe80a3d2e
Unknown <wallison_93@hotmail.com>
2017-09-23 17:34:21 -0300

    Criou atributos

--- src/main/java/com/mycompany/radecmaven/ClasseQualquer.java
+++ src/main/java/com/mycompany/radecmaven/ClasseQualquer.java
@@ -11,4 +11,34 @@ package com.mycompany.radecmaven;
  */
 public class ClasseQualquer {
     
+    int idade;
+    int cpf;
+    String nome;
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+    
 }
